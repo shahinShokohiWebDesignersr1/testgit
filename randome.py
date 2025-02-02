c3c5b7cd4728f5e073fadb987cbf995eb4a88c33
shahinShokohiWebDesignerr <shahinshokohi73@gmail.com>
2025-02-03 01:11:46 +0330

    peyda kardan aval
    tashkhis aval bodan unn

--- randome.py
+++ randome.py
@@ -1,2 +1,14 @@
-import random
-print(random.randint(1,100))
+number = int(input('Enter your number : '))
+if number < 2 :
+    print("No")
+elif number == 2 :
+    print("Yes")
+elif number % 2 == 0 :
+    print("No")
+else :
+    is_prime = True
+    for prime in range(3,int(number ** 0.5)+1,2):
+        if number % prime == 0:
+            is_prime = False
+            break
+    print("Yse" if is_prime else "No")
